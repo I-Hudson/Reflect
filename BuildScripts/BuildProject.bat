d8fc9d61ae6c077f2ab9f4c081517ad68f816ead
Ian Hudson <cuffy94@gmail.com>
2021-04-03 14:31:49 +0100

    Exe build script

--- BuildScripts/BuildProject.bat
+++ BuildScripts/BuildProject.bat
@@ -1,7 +1,13 @@
+@echo off
+
+set currentDisk=%cd:~0,2%
+set currentDir=%cd%
+
 cd /d C:
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE"
 
 call devenv.exe "F:\Users\Documents\SourceControl\Github\C++ Porjects\Reflect\Reflect.sln" /build Debug
 call devenv.exe "F:\Users\Documents\SourceControl\Github\C++ Porjects\Reflect\Reflect.sln" /build Release
 
-PAUSE
+cd /d %currentDisk%
+cd "%currentDir%"
---
+++ BuildScripts/Build_Reflect_EXE_zip.bat
@@ -0,0 +1,19 @@
+set /p version="Eneter version number or enter to continue: "
+
+SET PATH=%PATH%;"C:\Program Files\7-Zip"
+set exeLocation=%cd%\..\bin/Release-windows-x86_64\ReflectEXE
+set tempZipFolderName=tempZipFolder
+
+call "BuildProject.bat"
+
+
+xcopy /i "%exeLocation%\*.*" "%cd%/%tempZipFolderName%/"
+
+IF "%version%" == "" (
+    7z a Reflect_EXE.zip "%cd%/%tempZipFolderName%/*"
+) ELSE (
+    7z a Reflect_EXE_%version%.zip "%cd%/%tempZipFolderName%/*"
+)
+
+del /f /s /q "%cd%/tempZipFolder" 1>nul
+RMDIR /s /q "%cd%/tempZipFolder"
