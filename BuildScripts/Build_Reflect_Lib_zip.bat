b0a2b6e1c7553c0932c7c9157948d7bdcd2bd044
Ian Hudson <cuffy94@gmail.com>
2021-04-03 14:18:37 +0100

    Add small build lib script

---
+++ BuildScripts/Build_Reflect_Lib_zip.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set /p version="Eneter version number or enter to continue: "
+
+SET PATH=%PATH%;"C:\Program Files\7-Zip"
+set headerFiles=%cd%\..\Reflect\inc
+set libraryFilesDebug=%cd%\..\bin\Debug-windows-x86_64\Reflect
+set libraryFilesRelease=%cd%\..\bin/Release-windows-x86_64\Reflect
+set tempZipFolderName=tempZipFolder
+
+echo "%headerFiles%"
+echo "%libraryFilesDebug%"
+echo "%libraryFilesDebug%"
+
+xcopy /s /e /y /i "%headerFiles%\*.*" "%cd%/%tempZipFolderName%%/inc/"
+xcopy /i "%libraryFilesDebug%\*.*" "%cd%/%tempZipFolderName%/lib/x64/Debug"
+xcopy /i "%libraryFilesRelease%\*.*" "%cd%/%tempZipFolderName%/lib/x64/Release"
+
+IF "%version%" == "" (
+    7z a Reflect_Lib.zip "%cd%/%tempZipFolderName%/*"
+) ELSE (
+    7z a Reflect_Lib_%version%.zip "%cd%/%tempZipFolderName%/*"
+)
+
+del /f /s /q "%cd%/tempZipFolder" 1>nul
+RMDIR /s /q "%cd%/tempZipFolder"
